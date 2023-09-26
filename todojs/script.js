f1794ebd7bd7ad713c4cef4019f44b124e2cfea7
sheidaarvan <sheidaarvan1@gmail.com>
2023-09-26 15:03:01 +0330

    add filtertodos in todolist

--- todojs/script.js
+++ todojs/script.js
@@ -3,8 +3,13 @@ const todos = [];
 const todoInput = document.querySelector(".todo-input");
 const todoForm = document.querySelector(".todo-form");
 const todoList = document.querySelector(".todolist");
+const selectFilter = document.querySelector(".filter-todos");
 
+// events
 todoForm.addEventListener("submit", addNewTodo);
+selectFilter.addEventListener("change" , filterTodos);
+
+// function
 
 function addNewTodo(e) {
   e.preventDefault();
@@ -12,22 +17,49 @@ function addNewTodo(e) {
   if (!todoInput.value)return null;
   const newTodo = {
     id:Date.now(),
-    createdAt: new Date().toISOString(),
     title: todoInput.value,
     isCompleted: false,
   };
   todos.push(newTodo);
+  createTodos(todos);
 
+
+}
+function createTodos(todos){
   let result ="";
- todos.forEach((todo) =>{
-    result += `<li class="todo">
-    <p class="todo-title">${todo.title}</p>
-    <span class = "todo-ceratedAt>${new Date(todo.createdAt).toLocaleDateString("fa-IR")}</span>
-    <button class="check-button"><i class="fa-sharp fa-solid fa-check"></i></button>
-    <button class="trash-button"><i class="fa-solid fa-trash"></i></button>
-  </li>`;
-
- });
- todoList.innerHTML = result;
+  todos.forEach((todo) =>{
+     result += `<li class="todo">
+     <p class="todo-title">${todo.title}</p>
+     <button class="check-button"><i class="fa-sharp fa-solid fa-check"></i></button>
+     <button class="trash-button"><i class="fa-solid fa-trash"></i></button>
+   </li>`;
+ 
+  });
+  todoList.innerHTML = result;
+  todoInput.value =""
 }
 
+function filterTodos(e){
+  const filter = e.target.value;
+  switch(filter){
+    case"all":{
+      createTodos(todos);
+      break;
+
+    }
+    case"completed":{
+      const filteredTodos = todos.filter((t)=> t.isCompleted);
+      createTodos(filteredTodos);
+      break;
+
+    }case"uncompleted":{
+      const filteredTodos = todos.filter((t)=> !t.isCompleted);
+      createTodos(filteredTodos);
+      break;
+
+    }
+    default:
+      createTodos(todos);
+    
+  }
+}
